247c93799f18eeb2a78aad3f411a758fb0507380
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-09-18 20:34:23 -0300

    send data to card list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -12,18 +15,20 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://api.pokemontcg.io/v1/cards?name=pikachu')
-    .then(response => response.json())
-    .then(data => {
-      this.setState({ cards: data.cards})
-    });
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ cards: data.cards })
+      });
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.cards.map(card => <h1 key={card.id}> {card.name} </h1>)
-        }
+        <CardList name='list of pokemon cards'>
+          {
+            this.state.cards.map(card => <h1 key={card.id}> {card.name} </h1>)
+          }
+        </CardList>
       </div>
     );
   }
