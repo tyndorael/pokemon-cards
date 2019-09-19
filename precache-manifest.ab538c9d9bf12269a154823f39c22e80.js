3ffe1b332eaf2d89284a61c5288c461b803331d7
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-09-19 19:07:04 -0300

    Updates

---
+++ precache-manifest.ab538c9d9bf12269a154823f39c22e80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518f78e943a7595c7dd2b58387cff423",
+    "url": "/pokemon-cards/index.html"
+  },
+  {
+    "revision": "f48ef768cb9cffe948a0",
+    "url": "/pokemon-cards/static/css/main.43163c78.chunk.css"
+  },
+  {
+    "revision": "cb4f421ae2d0bd691388",
+    "url": "/pokemon-cards/static/js/2.8a41e32c.chunk.js"
+  },
+  {
+    "revision": "f48ef768cb9cffe948a0",
+    "url": "/pokemon-cards/static/js/main.87fc7bc3.chunk.js"
+  },
+  {
+    "revision": "c68c847c6c103b5f698a",
+    "url": "/pokemon-cards/static/js/runtime~main.16314df7.js"
+  }
+]);
