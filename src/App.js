69a96bd02b7fba7d8ba3234538456eae3c714449
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-11-11 12:02:10 -0300

    add debounce for search cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { SearchBox } from './components/search-box/search-box.component';
-import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from "./components/search-box/search-box.component";
+import { CardList } from "./components/card-list/card-list.component";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      searchField: '',
+      searchField: "",
       cards: []
     };
   }
 
   componentDidMount() {
-    fetch('https://api.pokemontcg.io/v1/cards?name=pikachu')
+    fetch("https://api.pokemontcg.io/v1/cards?name=pikachu")
       .then(response => response.json())
       .then(data => {
-        this.setState({ cards: data.cards })
+        this.setState({ cards: data.cards });
       });
   }
 
@@ -26,18 +26,37 @@ class App extends Component {
     fetch(`https://api.pokemontcg.io/v1/cards?name=${name}`)
       .then(response => response.json())
       .then(data => {
-        this.setState({ cards: data.cards })
+        this.setState({ cards: data.cards });
       });
-  }
+  };
+
+  // eslint-disable-next-line no-sequences
+  debounce = (a, b, c) => {
+    var d, e;
+    return function() {
+      function h() {
+        d = null;
+        c || (e = a.apply(f, g));
+      }
+      var f = this,
+        g = arguments;
+      return (
+        clearTimeout(d),
+        (d = setTimeout(h, b)),
+        c && !d && (e = a.apply(f, g)),
+        e
+      );
+    };
+  };
 
   // arrow function do context binding automatically
-  handleChange = e => {
-    this.setState({ searchField: e.target.value }, () => {
+  handleChange = this.debounce(e => {
+    this.setState({ searchField: e }, () => {
       if (this.state.searchField.length > 2) {
         this.searchCardsByPokemon(this.state.searchField);
       }
     });
-  }
+  }, 1000);
 
   render() {
     const { cards } = this.state;
@@ -47,7 +66,10 @@ class App extends Component {
         <h1>Pokemon Cards</h1>
         <SearchBox
           placeholder="search cards by set"
-          handleChange={this.handleChange} />
+          handleChange={e => {
+            this.handleChange(e.target.value);
+          }}
+        />
         <CardList cards={cards} />
       </div>
     );
